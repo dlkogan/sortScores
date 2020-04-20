b6a2040f4d7e369dc24a957ffb58e3de7de4fcd9
Danielle Kogan <kogandanielle@gmail.com>
2020-04-20 15:32:50 -0400

    some test specs! A loose solution

--- sortScores.js
+++ sortScores.js
@@ -1,6 +1,15 @@
 const sortScores = (unsortedScores, HIGHEST_POSSIBLE_SCORE) => {
-  //this 'hi' should be deleted, it's here to check testSpecs working
-  return "hi";
+  let scoreObj = {};
+  for (let i = 0; i < unsortedScores.length; i++) {
+    if (!scoreObj[HIGHEST_POSSIBLE_SCORE - unsortedScores[i]]) {
+      scoreObj[HIGHEST_POSSIBLE_SCORE - unsortedScores[i]] = unsortedScores[i];
+    } else {
+      scoreObj[HIGHEST_POSSIBLE_SCORE - unsortedScores[i]] += 1;
+    }
+  }
+  const objKeys = Object.keys(scoreObj);
+  console.log(scoreObj);
+  return objKeys.map(element => scoreObj[element]);
 }
 
 module.exports = sortScores;
--- testSpecs.js
+++ testSpecs.js
@@ -2,7 +2,13 @@ const { expect } = require('chai');
 const sortScores = require('./sortScores');
 
 describe('sortScores', () => {
-  it('mocha chai is it working test...', () => {
-    expect(sortScores([1,2,4], 10)).to.equal("hi");
+  it('If we have only one score, return just that score.', () => {
+    expect(sortScores([22], 100)).to.eql([22]);
   });
+  it('No scores in the array will exceed the highest number', () => {
+    expect(sortScores([22,2000], 100)).to.eql([22])
+  })
+  it('Receives an array and sorts it', () => {
+    expect(sortScores([89,2,47,99,4], 100)).to.eql([99,89,47,4,2])
+  })
 });
